808d6416be06a0f1b8ba88e80ede5fb02fdf6426
Basit Ali <opoali911@gmail.com>
2026-01-15 21:10:19 +0500

    learn a pattern and also learn how to make a modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./patterns/control&UncontrolComp/Counter";
 import AutoFoucsInput from "./patterns/control&UncontrolComp/AutoFoucsInput";
 import CounterRef from "./patterns/control&UncontrolComp/CounterRef";
@@ -6,8 +6,11 @@ import FeedbackForm from "./patterns/control&UncontrolComp/messy/FeedbackForm";
 import Form from "./patterns/control&UncontrolComp/Form";
 import MixtureOfBothHook from "./patterns/control&UncontrolComp/State&Ref";
 import Model from "./patterns/compondComponentPattern/messy/model";
-import './App.css'
+import "./App.css";
+import Modal from "./patterns/compondComponentPattern/modal";
 const App = () => {
+
+  const [isOpen,setIsOpen]=useState(false);
   return (
     <div className="flex flex-col items-center">
       {/* Controlled & Uncontrolled Pattern Start From Here */}
@@ -19,15 +22,29 @@ const App = () => {
       <MixtureOfBothHook /> */}
       {/* Controlled & Uncontrolled Pattern End From Here */}
 
-
-
-      <Model
+      {/* Compound component pattern start from here */}
+      {/* Messy code */}
+      {/* <Model
       title="Delete Account"
       body="Are you sure you want to delete your account?"
       primaryAction={<button>Delete</button>}
       secondaryAction={<button>Cancel</button>}
       
-      />
+      /> */}
+      {/* With pattern */}
+      <button onClick={()=>setIsOpen(true)} className="bg-amber-400 px-4 py-2 rounded-lg mt-10 font-bold">Open Modal </button>
+      <Modal isOpen={isOpen} onClose={()=>setIsOpen(false)}>
+        <Modal.Header>
+          <h1>Welcome!</h1>
+        </Modal.Header>
+        <Modal.Body>
+          <p>This is a modal built with the compound component pattern</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button>Close</button>
+          <button>Do Action</button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- src/patterns/compondComponentPattern/modal.jsx
+++ src/patterns/compondComponentPattern/modal.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Modal = () => {
+const Modal = ({ children,isOpen,onClose }) => {
+    if(!isOpen) return null;
   return (
-    <div>
-      
+    <div className="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex justify-center items-center">
+      <div className="bg-white rounded-2xl p-4 w-96 relative">
+        {children}
+      </div>
+      <div className="relative">
+        <button className="absolute -top-18 right-2 background-none border-none text-lg" onClick={onClose}>❌</button>
+      </div>
     </div>
-  )
+  );
+};
+
+function ModalHeader({children}){
+    return <div className='font-bold mb-4'>{children}</div>
+
+}
+function ModalBody({children}){
+    return <div className='modal-body'>{children}</div>
+
+}
+function ModalFooter({children}){
+    return <div className='flex justify-end gap-2 mt-4'>{children}</div>
+
 }
 
-export default Modal
+
+Modal.Header=ModalHeader;
+Modal.Body=ModalBody;
+Modal.Footer=ModalFooter;
+export default Modal;
